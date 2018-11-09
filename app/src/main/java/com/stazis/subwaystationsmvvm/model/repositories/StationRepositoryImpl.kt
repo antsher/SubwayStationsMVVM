6175c5cd8049c124a17caf145a0a19c1530b6541
Anton Sheremet <anton.sheremet@itechart-group.com>
2018-11-09 21:08:38 +0300

    finished implementing model, added a method for getting station basic info from server by station's name

--- app/src/main/java/com/stazis/subwaystationsmvvm/model/repositories/StationRepositoryImpl.kt
+++ app/src/main/java/com/stazis/subwaystationsmvvm/model/repositories/StationRepositoryImpl.kt
@@ -5,6 +5,7 @@ import com.stazis.subwaystationsmvvm.extensions.correctStations
 import com.stazis.subwaystationsmvvm.helpers.ConnectionHelper
 import com.stazis.subwaystationsmvvm.helpers.PreferencesHelper
 import com.stazis.subwaystationsmvvm.model.entities.Station
+import com.stazis.subwaystationsmvvm.model.entities.StationAdvancedInfo
 import com.stazis.subwaystationsmvvm.model.services.StationService
 import java.net.ConnectException
 import kotlin.coroutines.resume
@@ -27,22 +28,22 @@ class StationRepositoryImpl(
     private val firestore = FirebaseFirestore.getInstance()
 
     override suspend fun getStations(): List<Station> = if (preferencesHelper.retrieveBoolean(DATA_IN_FIRESTORE_KEY)) {
-        fetchStationsFromFirestore()
+        getStationsFromFirestore()
     } else {
         if (connectionHelper.isOnline()) {
-            fetchStationsFromServer()
+            getStationsFromServer()
         } else {
             throw ConnectException("No internet connection is present, and local database is empty")
         }
     }
 
-    private suspend fun fetchStationsFromServer(): List<Station> =
+    private suspend fun getStationsFromServer(): List<Station> =
         stationService.getStations().await().correctStations().apply {
-            writeBasicStationsToFirestore(this)
+            postBasicStationsToFirestore(this)
             createAdvancedStationsCollectionInFirestore(this)
         }
 
-    private fun writeBasicStationsToFirestore(stations: List<Station>) = stations.forEach {
+    private fun postBasicStationsToFirestore(stations: List<Station>) = stations.forEach {
         firestore.collection(STATION_BASIC_INFO_COLLECTION_NAME).document(it.name).set(it)
     }
 
@@ -50,21 +51,46 @@ class StationRepositoryImpl(
         firestore.collection(STATION_DETAILED_INFO_COLLECTION_NAME).document(it.name).set(mapOf("description" to ""))
     }
 
-    private suspend fun fetchStationsFromFirestore(): List<Station> = suspendCoroutine { continuation ->
+    private suspend fun getStationsFromFirestore(): List<Station> = suspendCoroutine {
         firestore.collection(STATION_BASIC_INFO_COLLECTION_NAME).get().addOnCompleteListener { task ->
             if (task.isSuccessful && !task.result!!.isEmpty) {
-                continuation.resume(task.result!!.toObjects(Station::class.java))
+                it.resume(task.result!!.toObjects(Station::class.java))
             } else {
-                continuation.resumeWithException(task.exception!!)
+                it.resumeWithException(task.exception!!)
             }
         }
     }
 
-    override suspend fun getStationDescription(name: String): String {
-        return ""
+    override suspend fun getStationBasicInfo(name: String): Station = suspendCoroutine {
+        firestore.collection(STATION_BASIC_INFO_COLLECTION_NAME).document(name).get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                it.resume(task.result!!.toObject(Station::class.java)!!)
+            } else {
+                it.resumeWithException(task.exception!!)
+            }
+        }
     }
 
-    override suspend fun updateStationDescription(name: String, description: String): String {
-        return ""
+    override suspend fun getStationDescription(name: String): String = suspendCoroutine {
+        firestore.collection(STATION_DETAILED_INFO_COLLECTION_NAME).document(name).get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                it.resume(task.result!!.toObject(StationAdvancedInfo::class.java)!!.description)
+            } else {
+                it.resumeWithException(task.exception!!)
+            }
+        }
+    }
+
+    override suspend fun updateStationDescription(name: String, description: String): String = suspendCoroutine {
+        firestore.collection(STATION_DETAILED_INFO_COLLECTION_NAME)
+            .document(name)
+            .update("description", description)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    it.resume("Station updated successfully!")
+                } else {
+                    it.resumeWithException(task.exception!!)
+                }
+            }
     }
 }
