9668330c1485edb1d8122ae530e6cdfce274f362
Anton Sheremet <anton.sheremet@itechart-group.com>
2018-11-09 21:11:12 +0300

    added method for getting station basic info to StationRepository

--- app/src/main/java/com/stazis/subwaystationsmvvm/model/repositories/StationRepository.kt
+++ app/src/main/java/com/stazis/subwaystationsmvvm/model/repositories/StationRepository.kt
@@ -5,6 +5,7 @@ import com.stazis.subwaystationsmvvm.model.entities.Station
 interface StationRepository {
 
     suspend fun getStations(): List<Station>
+    suspend fun getStationBasicInfo(name: String): Station
     suspend fun getStationDescription(name: String): String
     suspend fun updateStationDescription(name: String, description: String): String
 }
