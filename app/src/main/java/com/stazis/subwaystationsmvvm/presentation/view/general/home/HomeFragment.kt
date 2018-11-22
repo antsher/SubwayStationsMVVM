6ad42c11ee376f79f57d878b27ee9ca9c87c36be
Anton Sheremet <anton.sheremet@itechart-group.com>
2018-11-22 10:46:58 +0300

    added a button for navigating forward from HomeFragment

--- app/src/main/java/com/stazis/subwaystationsmvvm/presentation/view/general/home/HomeFragment.kt
+++ app/src/main/java/com/stazis/subwaystationsmvvm/presentation/view/general/home/HomeFragment.kt
@@ -5,10 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.stazis.subwaystationsmvvm.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
         inflater.inflate(R.layout.fragment_home, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) =
+        next.setOnClickListener { findNavController().navigate(R.id.station_map_dest) }
 }
