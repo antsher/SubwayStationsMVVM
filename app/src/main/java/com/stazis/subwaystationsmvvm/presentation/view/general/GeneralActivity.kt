d099c47f4249a2a8f70f1fecc9e665be38c9fcf3
Anton Sheremet <anton.sheremet@itechart-group.com>
2018-11-26 15:28:12 +0300

    fixed a bug when user could disable asking for permissions with "never ask again" checkbox and prevent app from working; now user is navigated into the app settings page

--- app/src/main/java/com/stazis/subwaystationsmvvm/presentation/view/general/GeneralActivity.kt
+++ app/src/main/java/com/stazis/subwaystationsmvvm/presentation/view/general/GeneralActivity.kt
@@ -2,10 +2,14 @@ package com.stazis.subwaystationsmvvm.presentation.view.general
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.firebase.analytics.FirebaseAnalytics
@@ -15,12 +19,14 @@ import com.stazis.subwaystationsmvvm.helpers.checkPermissionState
 import com.stazis.subwaystationsmvvm.helpers.requestPermission
 import kotlinx.android.synthetic.main.activity_general.*
 
+
 @SuppressLint("MissingPermission")
 class GeneralActivity : AppCompatActivity() {
 
     companion object {
 
         const val PERMISSION_REQUEST_CODE = 9001
+        const val SETTINGS_ACTIVITY_CODE = 9002
         private const val locationPermission = Manifest.permission.ACCESS_FINE_LOCATION
     }
 
@@ -30,10 +36,10 @@ class GeneralActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_general)
         bottomNavigation.setupWithNavController(findNavController(R.id.nav_host_fragment))
-        requestPermissions()
+        requestPermissionsIfNecessary()
     }
 
-    private fun requestPermissions() = when (checkPermissionState(this, locationPermission)) {
+    private fun requestPermissionsIfNecessary() = when (checkPermissionState(this, locationPermission)) {
         PermissionState.GRANTED -> {
         }
         PermissionState.NOT_GRANTED -> requestPermission(this, locationPermission)
@@ -43,12 +49,12 @@ class GeneralActivity : AppCompatActivity() {
         }
     }
 
-    private fun askNicelyForPermissions() = AlertDialog.Builder(this)
+    private fun askNicelyForPermissions(onDismiss: (() -> Unit)? = null) = AlertDialog.Builder(this)
         .setTitle(resources.getString(R.string.permissions_denied))
         .setMessage(resources.getString(R.string.cannot_run_without_location_permissions))
         .setNeutralButton("OK") { dialog, _ ->
             dialog.dismiss()
-            requestPermission(this, locationPermission)
+            onDismiss?.invoke() ?: requestPermission(this, locationPermission)
         }
         .create()
         .show()
@@ -57,9 +63,24 @@ class GeneralActivity : AppCompatActivity() {
         when (requestCode) {
             PERMISSION_REQUEST_CODE -> {
                 if ((grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED)) {
-                    askNicelyForPermissions()
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(this, locationPermission)) {
+                        askNicelyForPermissions()
+                    } else {
+                        askNicelyForPermissions {
+                            startActivityForResult(Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                                data = Uri.fromParts("package", packageName, null)
+                            }, SETTINGS_ACTIVITY_CODE)
+                        }
+                    }
                 }
             }
+            else -> throw IllegalArgumentException("Invalid request code!")
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            SETTINGS_ACTIVITY_CODE -> requestPermissionsIfNecessary()
             else -> throw IllegalArgumentException("Invalid request code!")
         }
     }
