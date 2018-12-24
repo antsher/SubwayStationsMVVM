7142803dc85bc3cf9777387e149e69fd827264bd
Anton Sheremet <anton.sheremet@itechart-group.com>
2018-12-24 18:24:09 +0300

    corrected LocationHelper

--- app/src/main/java/com/stazis/subwaystationsmvvm/helpers/LocationHelper.kt
+++ app/src/main/java/com/stazis/subwaystationsmvvm/helpers/LocationHelper.kt
@@ -5,7 +5,9 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
 import androidx.core.content.ContextCompat
+import com.google.android.gms.location.LocationServices
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.tasks.asDeferred
 
 class LocationHelper(private val context: Context) {
 
@@ -14,12 +16,12 @@ class LocationHelper(private val context: Context) {
             == PackageManager.PERMISSION_GRANTED
         ) {
             delay(1000)
-            Location("").apply {
-                latitude = 53.8851807
-                longitude = 27.5370945
-            }
-//            LocationServices.getFusedLocationProviderClient(context).lastLocation.asDeferred().await()
-//                ?: throw SecurityException("Location is unavailable!")
+//            Location("").apply {
+//                latitude = 53.8851807
+//                longitude = 27.5370945
+//            }
+            LocationServices.getFusedLocationProviderClient(context).lastLocation.asDeferred().await()
+                ?: throw SecurityException("Location is unavailable!")
         } else {
             throw SecurityException("Missing location permissions!")
         }
